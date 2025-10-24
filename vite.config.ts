b62d36265d2451d43ed2d3a32358e878d4a86cfe
tejaswa-jaiswal <tejasja2516e@gmail.com>
2025-10-24 10:39:02 +0530

    sixth

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
+  import path from "path";
 
   export default defineConfig({
     plugins: [react()],
+    base: './',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
